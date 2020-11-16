54694c1691dfd1a2cac7c17f043e398ffd811f74
leminhnguyen <leminhnguyen.bk@gmail.com>
2020-11-16 10:53:12 +0700

    Hung Nop BTVN_7

---
+++ BTVN_7/ex2/IShape.java
@@ -0,0 +1,6 @@
+package ex2;
+
+public interface IShape {
+    double getArea();
+    double getPerimeter();
+}
---
+++ BTVN_7/ex2/Rectangle.java
@@ -0,0 +1,22 @@
+package ex2;
+
+public class Rectangle implements IShape{
+
+    protected double length;
+    protected double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double getArea() {
+        return length * width;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return (length + width) * 2;
+    }
+}
